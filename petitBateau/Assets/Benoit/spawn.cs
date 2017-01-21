fbd21c708e9690f1cb7e97c188d24503eaa04891
benoit <petiteaubenoit@gmail.com>
2017-01-21 02:18:59 +0100

    legere modif spawn

--- petitBateau/Assets/Benoit/spawn.cs
+++ petitBateau/Assets/Benoit/spawn.cs
@@ -7,6 +7,7 @@ public class spawn : MonoBehaviour {
 	private float now;
 	private float xModifier = 12.0f;
 	private float yModifier = 5.0f;
+	private int lastRand = 0;
 
 	[SerializeField]
 	private float timerSpawn;
@@ -24,6 +25,12 @@ public class spawn : MonoBehaviour {
 	void Update () {
 		if (now + timerSpawn < Time.time) {
 			int rand = (int)(Random.value *7);
+
+			if (rand == lastRand)
+				rand = (rand+1)%7;
+
+			lastRand = rand;
+			
 			spawnRocks (rand);
 			now = Time.time;
 		}
@@ -33,7 +40,7 @@ public class spawn : MonoBehaviour {
 		float distance = Camera.main.ScreenToWorldPoint(new Vector2(0.0f,Screen.height)).y / 5;
 		switch (rand) {
 		case 0:
-			Instantiate (Rocher, new Vector2 (xModifier, Random.value * 10 * distance-4.5f), Quaternion.identity);
+			Instantiate (Rocher, new Vector2 (xModifier, Random.value * 9 * distance-4.5f), Quaternion.identity);
 			break;
 		case 1:
 			Instantiate (Rocher, new Vector2 (xModifier, 7.0f * distance-yModifier), Quaternion.identity);
