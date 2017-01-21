b0c51c0c7378b28bfce597898255fbaf842e1567
benoit <petiteaubenoit@gmail.com>
2017-01-21 16:03:26 +0100

    Scoring
    highscore enregistré

--- petitBateau/Assets/Ambroise/Script/setGameOver.cs
+++ petitBateau/Assets/Ambroise/Script/setGameOver.cs
@@ -24,7 +24,7 @@ public class setGameOver : MonoBehaviour {
 
     public void GameOver()
     {
-        GameOverText.text = "Game Over \nPush space";
+		GameOverText.text = "Game Over \nPush space\n\nScore : " + scoring.score+"\nHigh Score : "+scoring.highscore;
         isAlive = false;
     }
 
---
+++ petitBateau/Assets/Benoit/scoring.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoring : MonoBehaviour {
+
+	static public int highscore = 0;
+	static public int score = 0;
+	// Use this for initialization
+	void Start () {
+		highscore = PlayerPrefs.GetInt ("High Score", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (score > highscore && !GameObject.FindGameObjectWithTag ("GameController").GetComponent<setGameOver> ().getIsAlive ()) {
+			PlayerPrefs.SetInt ("High Score", score);
+			score = 0;
+		}
+	}
+}
