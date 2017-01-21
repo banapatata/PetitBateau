5656693a28f6f7a895dcffcb5d35f03d71cf35e7
damon3169 <ambjo@hotmail.fr>
2017-01-21 22:45:23 +0100

    Crate reparer

--- petitBateau/Assets/Ambroise/Script/CraftWaveBehavior.cs
+++ petitBateau/Assets/Ambroise/Script/CraftWaveBehavior.cs
@@ -13,6 +13,16 @@ public class CraftWaveBehavior : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private Sprite sprite1;
+
+    [SerializeField]
+    private Sprite sprite2;
+
+    [SerializeField]
+    private bool faischier = false;
+
+
     // Use this for initialization
     void Start()
     {
@@ -25,24 +35,65 @@ public class CraftWaveBehavior : MonoBehaviour
 
     }
 
+    public void HitByWave(Vector2 wave)
+    {
+        faischier = true;
+        GetComponent<Rigidbody2D>().AddForce(wave * 2.0f, ForceMode2D.Impulse);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        this.GetComponent<Collider2D>().isTrigger = false;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Wave")
+        if (other.gameObject.tag == "Wave")
         {
+            Debug.Log("test");
 
-            GetComponent<SpriteRenderer>().enabled = false;
-            float random1 = Random.Range(-1.0f, 1.0f);
-            float random2 = Random.Range(-1.0f, 1.0f);
-            Actualscrap = Instantiate(scrap, transform.position + new Vector3(0.02f, 0.02f), Quaternion.identity);
-            Actualscrap.GetComponent<Rigidbody2D>().AddForce(new Vector2(random1, random2) * speed);
-            random1 = Random.Range(-1.0f, 1.0f);
-            random2 = Random.Range(-1.0f, 1.0f);
-            Actualscrap = Instantiate(scrap, transform.position + new Vector3(-0.02f, 0.02f), Quaternion.identity);
-            Actualscrap.GetComponent<Rigidbody2D>().AddForce(new Vector2(random1, random2) * speed);
-            random1 = Random.Range(-1.0f, 1.0f);
-            random2 = Random.Range(-1.0f    , 1.0f   );
-            Actualscrap = Instantiate(scrap, transform.position + new Vector3(0.02f, -  0.02f), Quaternion.identity);
-            Actualscrap.GetComponent<Rigidbody2D>().AddForce(new Vector2(random1, random2) * speed);
+            if (other.gameObject.GetComponent<waveBehavior>().getDirection().magnitude < 6.0f)
+            {
+                if (!other.gameObject.GetComponent<waveBehavior>().getBoatAlreadyTouched())
+                {
+                    other.gameObject.GetComponent<waveBehavior>().setBoatAlreadyTouched(true);
+                    HitByWave(other.gameObject.GetComponent<waveBehavior>().getDirection());
+                }
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+                float random1 = Random.Range(-1.0f, 1.0f);
+                float random2 = Random.Range(-1.0f, 1.0f);
+                Actualscrap = Instantiate(scrap, transform.position + new Vector3(0.02f, 0.02f), Quaternion.identity);
+                Actualscrap.GetComponent<Rigidbody2D>().AddForce(new Vector2(random1, random2) * speed);
+                random1 = Random.Range(-1.0f, 1.0f);
+                random2 = Random.Range(-1.0f, 1.0f);
+                Actualscrap = Instantiate(scrap, transform.position + new Vector3(-0.02f, 0.02f), Quaternion.identity);
+                Actualscrap.GetComponent<Rigidbody2D>().AddForce(new Vector2(random1, random2) * speed);
+                Actualscrap.GetComponent<SpriteRenderer>().sprite = sprite1;
+                random1 = Random.Range(-1.0f, 1.0f);
+                random2 = Random.Range(-1.0f, 1.0f);
+                Actualscrap = Instantiate(scrap, transform.position + new Vector3(0.02f, -0.02f), Quaternion.identity);
+                Actualscrap.GetComponent<Rigidbody2D>().AddForce(new Vector2(random1, random2) * speed);
+                Actualscrap.GetComponent<SpriteRenderer>().sprite = sprite2;
+
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+       
+        if (other.gameObject.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<setGameOver>().GameOver();
+        }
+
+        if (other.gameObject.name == "ColliderLeft" || (other.gameObject.name == "ColliderRight" && faischier))
+        {
             Destroy(this.gameObject);
         }
     }
