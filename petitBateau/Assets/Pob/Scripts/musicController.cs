7361a6b1663d886d6dd4d9819cd5a85107e259b4
Pob-Fr <po.brocquet@gmail.com>
2017-01-22 14:03:25 +0100

    Sound phase one

---
+++ petitBateau/Assets/Pob/Scripts/musicController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicController : MonoBehaviour {
+
+	private static musicController instance = null;
+	public static musicController Instance {
+		get { return instance; }
+	}
+
+
+	// Use this for initialization
+	void Awake () {
+		if (instance != null && instance != this) {
+			Destroy (this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
